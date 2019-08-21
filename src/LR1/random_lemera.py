c22013283bbe176fa97dd517ee724c91d29c3929
Alexander <sasha.sokolovskiy99@gmail.com>
2019-08-21 22:41:56 +0300

    add LR 1

---
+++ src/LR1/random_lemera.py
@@ -0,0 +1,28 @@
+from LR1.constants import X0, A, M
+
+
+class LemerRandomGenerator:
+    def __init__(self, x0=X0, a=A, m=M):
+        self.x0 = x0
+        self.a = a
+        self.m = m
+        self.x = None
+
+    def get_next_random(self):
+        self.x = self.a * (self.x or self.x0) % self.m
+        return self.x / self.m
+
+    def get_random_num(self, x: int = None, a: int = None, m: int = None):
+        return (a or self.a) * (x or self.x or self.x0) % (m or self.m)
+
+    def calculate_period(self):
+        counter = 0
+
+        x = self.get_next_random()
+
+        while x != self.get_next_random():
+            counter += 1
+            if counter > 1_000_000:
+                raise Exception('Period > 1_000_000')
+
+        return counter
